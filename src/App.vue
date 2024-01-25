a47e0900c60e99931e07e02c1625c9837a39bebe
cnotv <giuseppe.leo@suse.com>
2024-01-25 21:16:28 +0100

    Allow to rotate pages with arrows using url paths

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyPress);
+});
+
+const handleKeyPress = (event: KeyboardEvent) => {
+  const name = router.currentRoute.value.name
+  const page = name ? +name.toString() : 0;
+
+  switch (event.key) {
+    case 'ArrowRight': {
+      if (page < 2) router.push(`/${page + 1}`);
+      break;
+    }
+
+    case 'ArrowLeft': {
+      if (page > 1) router.push(`/${page - 1}`);
+      break;
+    }
+  
+    default:
+      break;
+  }
+};
+
 </script>
 
 <template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SimplexWorker from '@/views/SimplexWorker/index.vue'
+import SimplexCached from '@/views/SimplexCached/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/1'
+    },
+    {
+      path: '/1',
+      name: '1',
+      component: SimplexWorker
+    },
+    {
+      path: '/2',
+      name: '2',
+      component: SimplexCached
     },
     {
       path: '/about',
