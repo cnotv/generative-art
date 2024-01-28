ce9d5409cc7d8ac6a58f555725a1abdd5ef20baa
cnotv <giuseppe.leo@suse.com>
2024-01-28 22:37:10 +0100

    Add recording capabilities for cube2

--- src/views/Projects/CubeMatrix2.vue
+++ src/views/Projects/CubeMatrix2.vue
@@ -3,20 +3,23 @@ import { ref, onMounted } from 'vue';
 import Stats from "stats.js";
 import * as dat from "dat.gui";
 import P5 from "p5";
+import { useRoute } from 'vue-router';
 
 const statsEl = ref(null)
 const canvas = ref(null)
 const stats = new Stats();
+const route = useRoute();
 
 onMounted(() => {
   new P5((p: P5) => init(
     p,
     statsEl.value as unknown as HTMLElement,
+    canvas.value as unknown as HTMLCanvasElement,
     stats as unknown as Stats
   ), statsEl.value!);
 })
 
-const init = (p: P5, statsEl: HTMLElement, stats: Stats): void => {
+const init = (p: P5, statsEl: HTMLElement, canvas: HTMLCanvasElement, stats: Stats): void => {
   // FPS stats
   stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
   statsEl!.appendChild(stats.dom);
@@ -26,6 +29,7 @@ const init = (p: P5, statsEl: HTMLElement, stats: Stats): void => {
   let rotation = 0;
   let amountX = 50;
   let amountY = 50;
+  const totalFrames = 500;
 
   const config = {
     size: 20,
@@ -39,6 +43,30 @@ const init = (p: P5, statsEl: HTMLElement, stats: Stats): void => {
   control.add(config, "size").min(10).max(50);
   control.add(config, "speed").min(1).max(100);
   control.add(config, "gap").min(1).max(3);
+
+    // Outside your setup function
+  let chunks: Blob[] = [];
+  let mediaRecorder: MediaRecorder;
+
+  if (route.query.record === 'true') {
+    let stream = canvas.captureStream(30); // 30 FPS
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = function (e) {
+      chunks.push(e.data);
+      console.log(e.data)
+    };
+
+    mediaRecorder.onstop = function (e) {
+      let blob = new Blob(chunks, { 'type': 'video/mp4' });
+      let url = URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      a.href = url;
+      a.download = 'animation.mp4';
+      a.click();
+    };
+    mediaRecorder.start();
+  }
 
   const drawGeometry = (p: P5, x: number, y: number) => {
     p.push(); // Point changes to this instance
@@ -84,6 +112,13 @@ const init = (p: P5, statsEl: HTMLElement, stats: Stats): void => {
     for (let x = 0; x < amountX; x++) {
       for (let y = 0; y < amountY; y++) {
         drawGeometry(p, x, y);
+      }
+    }
+    if (route.query.record === 'true') {
+      if (p.frameCount >= totalFrames) {
+        if (mediaRecorder) {
+          mediaRecorder.stop();
+        }
       }
     }
     stats.end();
