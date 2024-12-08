c5f89fd9e95199f7008647afd26c1a37622b01c7
cnotv <giuseppe.leo@suse.com>
2024-12-09 00:00:50 +0100

    Add animated scene

---
+++ src/views/Experiments/ModelAnimation.vue
@@ -0,0 +1,220 @@
+<script setup lang="ts">
+import * as THREE from 'three';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { video } from '@/utils/video';
+import { controls } from '@/utils/control';
+import { stats } from '@/utils/stats';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import RAPIER from '@dimforge/rapier3d';
+import terrainTextureAsset from '@/assets/terrain.jpg';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+
+type Model = THREE.Group<THREE.Object3DEventMap>
+
+const statsEl = ref(null)
+const canvas = ref(null)
+const route = useRoute();
+const groundSize = [1000.0, 0.1, 1000.0] as [number, number, number];
+const sphereSize = () => Math.random() * 0.5 + 0.5;
+const groundPosition = [1, -1, 1] as [number, number, number];
+let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+let world = new RAPIER.World(gravity);
+
+/**
+ * Reflection
+ * https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_cubemap.html
+ * https://threejs.org/examples/#webgl_animation_skinning_ik
+ * https://paulbourke.net/panorama/cubemaps/
+ */
+const cubeFaces = ['px', 'nx', 'py', 'ny', 'pz', 'nz'];
+const urls = cubeFaces.map(code => new URL(`../../assets/cubemaps/stairs/${code}.jpg`, import.meta.url).href);
+const reflection = new THREE.CubeTextureLoader().load( urls );
+
+onMounted(() => {
+  init(
+    canvas.value as unknown as HTMLCanvasElement,
+    statsEl.value as unknown as HTMLElement,
+  ), statsEl.value!;
+})
+
+const init = async (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
+  const config = {
+    // size: 50,
+  }
+  stats.init(route, statsEl);
+  controls.create(config, route, {
+  }, () => {
+    setup()
+  });
+
+  const setup = async () => {
+    const renderer = getRenderer(canvas);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const scene = new THREE.Scene();
+    const orbit = new OrbitControls(camera, renderer.domElement);
+
+    camera.position.z = -50;
+    camera.position.y = 10;
+
+    createLights(scene);
+    getGround(groundSize, groundPosition, scene, world);
+    const { model, mixer } = await loadAnimatedModel();
+    scene.add(model);
+
+    video.record(canvas, route);
+
+    function animate() {
+      stats.start(route);
+      const frame = requestAnimationFrame(animate);
+      playAnimationModel(mixer, frame);
+      orbit.update();
+
+      renderer.render( scene, camera );
+      video.stop(renderer.info.render.frame ,route);
+      stats.end(route);
+    }
+    animate();
+  }
+  setup();
+}
+
+const playAnimationModel = (mixer: THREE.AnimationMixer, frame: number) => {
+  if (mixer) {
+    mixer.timeScale = 1;
+    if (frame % 7 === 0) {
+      mixer.update(frame);
+    }
+  }
+} 
+
+const getRenderer = (canvas: HTMLCanvasElement) => {
+  const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xaaaaff); // Set background color to black
+  renderer.shadowMap.enabled = true; // Enable shadow maps
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Use soft shadows
+  return renderer;
+}
+
+const createLights = (scene: THREE.Scene) => {
+  // Add directional light with shadows
+  const directionalLight = new THREE.DirectionalLight(0xffffaa, 1.2);
+  directionalLight.position.set(5, 5, 5);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 2048;
+  directionalLight.shadow.mapSize.height = 2048;
+  directionalLight.shadow.camera.near = 0.5;
+  directionalLight.shadow.camera.far = 50;
+  directionalLight.shadow.camera.left = -10;
+  directionalLight.shadow.camera.right = 10;
+  directionalLight.shadow.camera.top = 10;
+  directionalLight.shadow.camera.bottom = -10;
+  directionalLight.shadow.bias = -0.0001;
+  scene.add(directionalLight);
+
+  const ambient = new THREE.AmbientLight( 0xffffff, 0.3 );
+  ambient.position.set(5, 5, 5);
+  scene.add( ambient )
+}
+
+/**
+ * Get default textures
+ * @param img 
+ * @returns 
+ */
+const getTextures = (img: string) => {
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(img);
+
+  // Adjust the texture offset and repeat
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.offset.set(1, 1); // Offset the texture by 50%
+  texture.repeat.set(1, 1); // Repeat the texture 0.5 times in both directions
+
+  return texture;
+}
+
+/**
+ * Create scene ground with physics, texture, and shadow
+ * @param size 
+ * @param position 
+ * @param scene 
+ * @param world 
+ */
+const getGround = (
+  size: [number, number, number],
+  position: [number, number, number],
+  scene: THREE.Scene,
+  world: RAPIER.World
+) => {
+  // Create and add model
+  const terrainTexture = getTextures(terrainTextureAsset);
+  const material = new THREE.MeshPhysicalMaterial({
+    map: terrainTexture,
+    reflectivity: 0.3,
+  });
+  const geometry = new THREE.BoxGeometry( ...size);
+  const ground = new THREE.Mesh(geometry, material);
+  ground.position.set(...position);
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  // Create a dynamic rigid-body.
+  let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(...position);
+
+  // Create a cuboid collider attached to the dynamic rigidBody.
+  let colliderDesc = RAPIER.ColliderDesc.cuboid(...size).setTranslation(...position);
+  let collider = world.createCollider(colliderDesc);
+
+  return { ground, collider };
+}
+
+const loadFBX = () => {
+  return new Promise((resolve, reject) => {
+    const loader = new FBXLoader();
+    loader.setPath('../../../public/');
+    loader.load('low_poly.glb', (fbx) => {
+      resolve(fbx);
+    }, undefined, reject);
+  });
+}
+
+/**
+ * Return threeJS valid 3D model
+ */
+const loadGLTF = (): Promise<{ model: Model, gltf: any}> => {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader();
+    loader.setPath('../../../public/');
+    loader.load('low_poly.glb', (gltf) => {
+      resolve({model: gltf.scene, gltf});
+    }, undefined, reject);
+  });
+}
+
+const loadAnimatedModel = async () => {
+  const { model, gltf } = await loadGLTF();
+  model.castShadow = true;
+  model.receiveShadow = false; //default
+  model.position.set(0, 0, 0);
+  model.scale.set(8, 8, 8);
+  // Flip the model
+  model.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+
+  // Add animation
+  const mixer = new THREE.AnimationMixer(model)
+  const action = mixer.clipAction(gltf.animations[0]);
+  action.play();
+
+  return { model, mixer }
+}
+</script>
+
+<template>
+  <div ref="statsEl"></div>
+  <canvas ref="canvas"></canvas>
+</template>
+
