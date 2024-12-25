36a6a40e6a7e4ff933c7de08f060f863351224e7
cnotv <giuseppe.leo@suse.com>
2024-12-26 00:18:16 +0100

    Allow to define blade grass shape with controls

--- src/views/Generator/Grass.vue
+++ src/views/Generator/Grass.vue
@@ -8,6 +8,7 @@ import { stats } from '@/utils/stats';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { createLights, getRenderer, instanceMatrixMesh, setThirdPersonCamera } from '@/utils/threeJs';
 import { times } from '@/utils/lodash';
+import { step } from 'three/examples/jsm/nodes/Nodes.js';
 
 interface GenerateConfig {
   density: number;
@@ -18,6 +19,28 @@ interface GenerateConfig {
   offsetX: number;
   offsetY: number;
   offsetZ: number;
+  lengthCurve: {
+    base1: number;
+    base2: number;
+    base3: number;
+    mid1: number;
+    mid2: number;
+    mid3: number;
+    tip1: number;
+    tip2: number;
+    tip3: number;
+  };
+  sideCurve: {
+    base1: number;
+    base2: number;
+    base3: number;
+    mid1: number;
+    mid2: number;
+    mid3: number;
+    tip1: number;
+    tip2: number;
+    tip3: number;
+  };
 }
 
 const statsEl = ref(null)
@@ -49,6 +72,28 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
       offsetX: -2,
       offsetY: 0,
       offsetZ: 0,
+      lengthCurve: {
+        base1: 0.01,
+        base2: 0.00,
+        base3: 0.05,
+        mid1: 0.00,
+        mid2: 0.4,
+        mid3: 0.15,
+        tip1: 0.0,
+        tip2: 0.7,
+        tip3: 0.005,
+      },
+      sideCurve: {
+        base1: 0.05,
+        base2: 0.0,
+        base3: 0.0,
+        mid1: 0.04,
+        mid2: 0.25,
+        mid3: 0.0,
+        tip1: 0,
+        tip2: 1,
+        tip3: 0,
+      },
     },
     camera: {
       fixed: true,
@@ -74,14 +119,36 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
   // Set configuration and start setup
   controls.create(config, route, {
     grass: {
-      density: { min: 0 },
-      scale: { min: 0 },
-      scaleMin: { min: 0 },
-      area: { min: 0 },
-      points: { min: 0 },
-      offsetX: {},
-      offsetY: {},
-      offsetZ: {},
+      // density: { min: 0 },
+      // scale: { min: 0 },
+      // scaleMin: { min: 0 },
+      // area: { min: 0 },
+      // points: { min: 0 },
+      // offsetX: {},
+      // offsetY: {},
+      // offsetZ: {},
+      lengthCurve: {
+        base1: { min: 0, step: 0.01 },
+        base2: { min: 0, step: 0.01 },
+        base3: { min: 0, step: 0.01 },
+        mid1: { min: 0, step: 0.01 },
+        mid2: { min: 0, step: 0.01 },
+        mid3: { min: 0, step: 0.01 },
+        tip1: { min: 0, step: 0.01 },
+        tip2: { min: 0, step: 0.01 },
+        tip3: { min: 0, step: 0.01 },
+      },
+      sideCurve: {
+        base1: { min: 0, step: 0.01 },
+        base2: { min: 0, step: 0.01 },
+        base3: { min: 0, step: 0.01 },
+        mid1: { min: 0, step: 0.01 },
+        mid2: { min: 0, step: 0.01 },
+        mid3: { min: 0, step: 0.01 },
+        tip1: { min: 0, step: 0.01 },
+        tip2: { min: 0, step: 0.01 },
+        tip3: { min: 0, step: 0.01 },
+      },
     },
     camera: {
       fixed: {},
@@ -111,19 +178,19 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
       ...generateProps(config.grass),
       {
         position: [3, 1, 0],
-        rotation: [0, -0.5, 0],
+        rotation: [0, -0.45, 0],
         scale: [config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin]
-      }, // Flat front
+      }, // Flat front view
       {
         position: [3, -1, 0],
         rotation: [0, 1, 0],
         scale: [config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin]
-      },// Flat side
+      },// Flat side view
       {
         position: [3, -3, 0],
         rotation: [0, 0, 0],
         scale: [config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin, config.grass.scale + config.grass.scaleMin]
-      },// Flat side
+      },// Perspective view
     ] as ModelOptions[]
 
     camera.position.set(-30, 25, 30);
@@ -164,25 +231,48 @@ const generateProps = (config: GenerateConfig) => times(config.density * config.
   }
 });
 
+
+// const lengthCurve = new THREE.CatmullRomCurve3([
+//     // new THREE.Vector3(0.01, 0, 0.05),  // Base
+//     // new THREE.Vector3(0, 0.40, 0.15),  // Midpoint 1
+//     // // new THREE.Vector3(0, 0.50, 0.1),  // Midpoint 2
+//     // // new THREE.Vector3(0, 0.60, 0.1),  // Midpoint 3
+//     // new THREE.Vector3(0, 0.7, 0.05)  // Tip
+//     new THREE.Vector3(config.lengthCurve.base, 0, config.lengthCurve.mid1),  // Base
+//     new THREE.Vector3(0, config.lengthCurve.mid2, config.lengthCurve.mid3),  // Midpoint 1
+//     // new THREE.Vector3(0, 0.50, 0.1),  // Midpoint 2
+//     // new THREE.Vector3(0, 0.60, 0.1),  // Midpoint 3
+//   ]);
+
+//   // Define the control points for the side curve (curvature on the sides)
+//   // Vector values respectively: Width blade
+//   const sideCurve = new THREE.CatmullRomCurve3([
+//     new THREE.Vector3(0.05, 0, 0),  // Base
+//     new THREE.Vector3(0.04, 0.25, 0),  // Midpoint 1
+//     // new THREE.Vector3(0.03, 0.5, 0),  // Midpoint 2
+//     // new THREE.Vector3(0.02, 0.75, 0),  // Midpoint 3
+//     new THREE.Vector3(0, 1, 0)  // Tip
+//   ]);
+
 const getGrass = (config: GenerateConfig) => {
   // Define the control points for the length curve (curvature along the length)
   // Vector values respectively: bend sides, blade silhouette, bend front 
   const lengthCurve = new THREE.CatmullRomCurve3([
-    new THREE.Vector3(0.01, 0, 0.05),  // Base
-    new THREE.Vector3(0, 0.40, 0.15),  // Midpoint 1
+    new THREE.Vector3(config.lengthCurve.base1, config.lengthCurve.base2, config.lengthCurve.base3),  // Base
+    new THREE.Vector3(config.lengthCurve.mid1, config.lengthCurve.mid2, config.lengthCurve.mid3),  // Midpoint 1
     // new THREE.Vector3(0, 0.50, 0.1),  // Midpoint 2
     // new THREE.Vector3(0, 0.60, 0.1),  // Midpoint 3
-    new THREE.Vector3(0, 0.7, 0.05)  // Tip
+    new THREE.Vector3(config.lengthCurve.tip1, config.lengthCurve.tip2, config.lengthCurve.tip3)  // Tip
   ]);
 
   // Define the control points for the side curve (curvature on the sides)
   // Vector values respectively: Width blade
   const sideCurve = new THREE.CatmullRomCurve3([
-    new THREE.Vector3(0.05, 0, 0),  // Base
-    new THREE.Vector3(0.04, 0.25, 0),  // Midpoint 1
+    new THREE.Vector3(config.sideCurve.base1, config.sideCurve.base2, config.sideCurve.base3),  // Base
+    new THREE.Vector3(config.sideCurve.mid1, config.sideCurve.mid2, config.sideCurve.mid3),  // Midpoint 1
     // new THREE.Vector3(0.03, 0.5, 0),  // Midpoint 2
     // new THREE.Vector3(0.02, 0.75, 0),  // Midpoint 3
-    new THREE.Vector3(0, 1, 0)  // Tip
+    new THREE.Vector3(config.sideCurve.tip1, config.sideCurve.tip2, config.sideCurve.tip3)  // Tip
   ]);
 
   // Define the control points for the length curve (curvature along the length)
