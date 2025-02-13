827c44554ea5c3c44994dcb1ffa5124b5f70fdc2
cnotv <giuseppe.leo@suse.com>
2025-02-13 22:17:08 +0100

    Get platform based on experiment cases; Set experiment positions programmatically

--- src/views/Experiments/PhysicExamples.vue
+++ src/views/Experiments/PhysicExamples.vue
@@ -15,7 +15,6 @@ const canvas = ref(null)
 const route = useRoute();
 let gravity = { x: 0.0, y: -9.81, z: 0.0 };
 let world = new RAPIER.World(gravity);
-const models = [] as { mesh: THREE.Mesh<any>, rigidBody: RAPIER.RigidBody, collider: RAPIER.Collider }[];
 
 onMounted(() => {
   init(
@@ -46,16 +45,35 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
     const { renderer, scene, camera, clock, orbit } = getEnvironment(canvas);
     createLights(scene, {directionalLightIntensity: config.directional.intensity });
     // getGround(scene, world, { worldSize: 1000.0 });
-    times(9, (i) => {
-      const x = -20 + 20 * i;
+
+    const bouncyBall = getBall(scene, world)
+    const pingPongBall = getBall(scene, world)
+    const bowlingBall = getBall(scene, world)
+    const paperBall = getBall(scene, world)
+    const tennisBall = getBall(scene, world)
+    const rubberBall = getBall(scene, world)
+    const experiments = [
+      bouncyBall,
+      pingPongBall,
+      bowlingBall,
+      paperBall,
+      tennisBall,
+      rubberBall
+    ];
+
+    times(experiments.length, (i) => {
+      const x = -20 + (i % 3) * 20;
+      const y = 5 + Math.floor(i / 3) * -20;
       getCube(scene, world, {
         color: 0xaaaaaa,
         size: [10, 0.2, 3],
-        position: [x, 8, 0],
+        position: [x, y, 0],
         rotation: [0, -0.7, 10],
       })
+
+      experiments[i].initialValues.position = [x, y + 10, 0]
+      experiments[i].rigidBody.setTranslation({ x, y: y + 10, z: 0 }, true);
     });
-    models.push(getBall(scene, world, { position: [10, 10, 0]}));
 
     video.record(canvas, route);
 
@@ -65,20 +83,20 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
       const frame = requestAnimationFrame(animate);
       world.step();
 
-      models.forEach(({ mesh, rigidBody }) => {
+      experiments.forEach(({ mesh, rigidBody }) => {
         let position = rigidBody.translation();
         mesh.position.set(position.x, position.y, position.z);
         let rotation = rigidBody.rotation();
         mesh.rotation.set(rotation.x, rotation.y, rotation.z);
       });
-      // models[0].mesh.position.y -= 0.1;
+      // experiments[0].mesh.position.y -= 0.1;
       animateTimeline([{
-        interval: [2, 200], action: () => {
-          models.forEach(({ rigidBody }) => {
+        interval: [10, 200], action: () => {
+          experiments.forEach(({ rigidBody, initialValues: { position: [x, y, z]} }) => {
             rigidBody.resetForces(true);
             rigidBody.resetTorques(true);
             rigidBody.setRotation({ x: 0, y: 0, z: 0, w: 1 }, true);
-            rigidBody.setTranslation({ x: 20, y: 15, z: 0 }, true);
+            rigidBody.setTranslation({ x, y, z }, true);
           });
         }},
       ], frame);
