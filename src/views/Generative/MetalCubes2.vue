b7bb084178799fba2888d709151d86219ebdb866
cnotv <giuseppe.leo@suse.com>
2025-05-11 00:42:51 +0200

    Correct import in other project

--- src/views/Generative/MetalCubes2.vue
+++ src/views/Generative/MetalCubes2.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import * as THREE from 'three';
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import { useRoute } from 'vue-router';
-import { video } from '@/utils/video';
-import { controls } from '@/utils/control';
-import { stats } from '@/utils/stats';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { animateTimeline, createLights, instanceMatrixMesh } from '@/utils/threeJs';
-import { getRoundedBox } from '@/utils/custom-models';
-import { times } from '@/utils/lodash';
+import * as THREE from "three";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { useRoute } from "vue-router";
+import { video } from "@/utils/video";
+import { controls } from "@/utils/control";
+import { stats } from "@/utils/stats";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { animateTimeline } from "@/utils/animation";
+import { createLights, instanceMatrixMesh } from "@/utils/threeJs";
+import { getRoundedBox } from "@/utils/custom-models";
+import { times } from "@/utils/lodash";
 
-const statsEl = ref(null)
-const canvas = ref(null)
+const statsEl = ref(null);
+const canvas = ref(null);
 const route = useRoute();
 const animationId = ref(0);
 
@@ -21,16 +22,19 @@ const animationId = ref(0);
  * https://threejs.org/examples/#webgl_animation_skinning_ik
  * https://paulbourke.net/panorama/cubemaps/
  */
-const cubeFaces = ['px', 'nx', 'py', 'ny', 'pz', 'nz'];
-const urls = cubeFaces.map(code => new URL(`../../assets/cubemaps/stairs/${code}.jpg`, import.meta.url).href);
-const reflection = new THREE.CubeTextureLoader().load( urls );
+const cubeFaces = ["px", "nx", "py", "ny", "pz", "nz"];
+const urls = cubeFaces.map(
+  (code) => new URL(`../../assets/cubemaps/stairs/${code}.jpg`, import.meta.url).href
+);
+const reflection = new THREE.CubeTextureLoader().load(urls);
 
 onMounted(() => {
   init(
-    canvas.value as unknown as HTMLCanvasElement,
-    statsEl.value as unknown as HTMLElement,
-  ), statsEl.value!;
-})
+    (canvas.value as unknown) as HTMLCanvasElement,
+    (statsEl.value as unknown) as HTMLElement
+  ),
+    statsEl.value!;
+});
 
 onBeforeUnmount(() => {
   if (animationId.value) {
@@ -38,57 +42,85 @@ onBeforeUnmount(() => {
   }
 });
 
-const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
+const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement) => {
   const config = {
     size: 20,
     intervals: 90,
     cameraDistance: 50,
     reflection: true,
-    reflectivity: .2,
+    reflectivity: 0.2,
     roughness: 0.3,
     metalness: 0.8,
     transmission: 1,
-  }
+  };
   // Set stats
   stats.init(route, statsEl);
 
   // Set configuration and start setup
-  controls.create(config, route, {
-    size: {},
-    reflectivity: {},
-    roughness: {},
-    metalness: {},
-    transmission: {},
-  }, () => {
-    if (animationId.value) {
-      cancelAnimationFrame(animationId.value);
+  controls.create(
+    config,
+    route,
+    {
+      size: {},
+      reflectivity: {},
+      roughness: {},
+      metalness: {},
+      transmission: {},
+    },
+    () => {
+      if (animationId.value) {
+        cancelAnimationFrame(animationId.value);
+      }
+      setup();
     }
-    setup()
-  });
+  );
   const positions = generatePositions();
 
   const setup = () => {
     // Init canvas
     const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x000000); // Set background color to black
     const scene = new THREE.Scene();
-    
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 400);
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      400
+    );
     const orbit = new OrbitControls(camera, renderer.domElement);
     camera.position.set(config.size / 2, 0, config.cameraDistance);
     // camera.lookAt(20, 0, 0);
-  
+
     // Define the timeline and animations
     const cubesTimeline: Timeline[] = [
-      { interval: [config.intervals, config.intervals], action: (cube) => { cube.rotation.x += THREE.MathUtils.degToRad(1); } },
-      { delay: config.intervals, interval: [config.intervals, config.intervals], action: (cube) => { cube.rotation.y  += THREE.MathUtils.degToRad(1); } },
+      {
+        interval: [config.intervals, config.intervals],
+        action: (cube) => {
+          cube.rotation.x += THREE.MathUtils.degToRad(1);
+        },
+      },
+      {
+        delay: config.intervals,
+        interval: [config.intervals, config.intervals],
+        action: (cube) => {
+          cube.rotation.y += THREE.MathUtils.degToRad(1);
+        },
+      },
     ];
 
     const cubeMesh = getCube(config);
-    const cubes = instanceMatrixMesh(cubeMesh, scene, positions.map(position => ({ position, scale: [config.size, config.size, config.size] })));
+    const cubes = instanceMatrixMesh(
+      cubeMesh,
+      scene,
+      positions.map((position) => ({
+        position,
+        scale: [config.size, config.size, config.size],
+      }))
+    );
 
-    createLights(scene, { directionalLightIntensity: 10});
+    createLights(scene, { directionalLightIntensity: 10 });
 
     // Start recording video if URL has query string ?video=true
     video.record(canvas, route);
@@ -103,18 +135,21 @@ const init = (canvas: HTMLCanvasElement, statsEl: HTMLElement, ) => {
       //   animateTimeline(cubesTimeline, animationId.value, cube);
       // });
 
-      animateTimeline([{ start: 0, action: () => camera.position.z += -1 }], animationId.value);
+      animateTimeline(
+        [{ start: 0, action: () => (camera.position.z += -1) }],
+        animationId.value
+      );
       renderer.render(scene, camera);
 
       // Stop recording after X frames
       video.stop(renderer.info.render.frame, route);
-      
+
       stats.end(route); // Stats counter finish
-    }
+    };
     animate();
-  }
+  };
   setup();
-}
+};
 
 const cubesUpdates = (
   cubes: THREE.Mesh[],
@@ -122,8 +157,7 @@ const cubesUpdates = (
   scene: THREE.Scene,
   positions: CoordinateTuple[],
   config: any
-) => {
-}
+) => {};
 
 const generatePositions = (): CoordinateTuple[] => {
   const size = 20;
@@ -141,11 +175,11 @@ const generatePositions = (): CoordinateTuple[] => {
   }).flat();
 
   return positions;
-}
+};
 
 const getCube = (
   config: any,
-  { size, position }: { size?: CoordinateTuple, position?: CoordinateTuple } = {}
+  { size, position }: { size?: CoordinateTuple; position?: CoordinateTuple } = {}
 ) => {
   const depth = 0.02 * (size ? size[0] : 1);
   const geometry = getRoundedBox(size ?? [1, 1, 1], depth, 10);
@@ -160,13 +194,12 @@ const getCube = (
   const cube = new THREE.Mesh(geometry, material);
 
   if (position) cube.position.set(...position);
-  
+
   return cube;
-}
+};
 </script>
 
 <template>
   <div ref="statsEl"></div>
   <canvas ref="canvas"></canvas>
 </template>
-
