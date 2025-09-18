4043c3c15d2d6b27b87a1e1f00aff3de6165f901
cnotv <giuseppe.leo@suse.com>
2025-09-18 05:00:58 +0200

    Add circles visualizer

--- src/views/Experiments/Visualizer/index.vue
+++ src/views/Experiments/Visualizer/index.vue
@@ -14,7 +14,7 @@ const statsEl = ref(null);
 const canvas = ref(null);
 const audioElement = ref();
 const route = useRoute();
-const currentVisualizer = ref('frequencies'); // Use first available visualizer
+const currentVisualizer = ref('logo'); // Use first available visualizer
 const visualizer = ref(getVisualizer(currentVisualizer.value) as VisualizerSetup);
 const visualizerObjects = ref({} as Record<string, any>);
 let switchVisualizerFunction: ((name: string) => void) | null = null;
