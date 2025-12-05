ac8c4cf4ed21811b45388784f03b988c6aa9917a
cnotv <giuseppe.leo@suse.com>
2025-12-05 16:01:50 +0100

    Isolate configuration for tooltest

--- src/views/Experiments/ToolsTest.vue
+++ src/views/Experiments/ToolsTest.vue
@@ -3,6 +3,25 @@ import { onMounted, ref } from "vue";
 import { getTools, getModel } from "@webgametoolkit/threejs";
 import { updateAnimation } from "@webgametoolkit/animation";
 
+const chameleonConfig = {
+  position: [0, -0.75, 0],
+  scale: [0.05, 0.05, 0.05],
+  restitution: -10,
+  boundary: 0.5,
+  type: "kinematicPositionBased",
+  hasGravity: false,
+  castShadow: true,
+  receiveShadow: true,
+  animations: "chameleon_animations.fbx",
+};
+
+const setupConfig = {
+  camera: { position: [0, 5, 20] },
+  ground: { size: 10000, color: 0x99cc99 },
+  sky: { size: 500, color: 0x87ceeb },
+  lights: { directional: { intensity: 0.1 } },
+};
+
 const canvas = ref(null);
 const init = async () => {
   const { setup, animate, scene, world, getDelta } = await getTools({
@@ -12,24 +31,9 @@ const init = async () => {
   });
 
   await setup({
-    config: {
-      camera: { position: [0, 5, 20] },
-      ground: { size: 10000, color: 0x99cc99 },
-      sky: { size: 500, color: 0x87ceeb },
-      lights: { directional: { intensity: 0.1 } },
-    },
+    config: setupConfig,
     defineSetup: async () => {
-      const chameleon = await getModel(scene, world, "chameleon.fbx", {
-        position: [0, -0.75, 0],
-        scale: [0.05, 0.05, 0.05],
-        restitution: -10,
-        boundary: 0.5,
-        type: "kinematicPositionBased",
-        hasGravity: false,
-        castShadow: true,
-        receiveShadow: true,
-        animations: "chameleon_animations.fbx",
-      });
+      const chameleon = await getModel(scene, world, "chameleon.fbx", chameleonConfig);
 
       animate({
         beforeTimeline: () => {},
