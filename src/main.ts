a16c950e2a5be1010f60457bd5c7ef64e9cfd13f
cnotv <giuseppe.leo@suse.com>
2025-12-09 00:36:56 +0100

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import * as Sentry from "@sentry/vue";
 
 const app = createApp(App)
+
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 app.use(createPinia())
 app.use(router)
