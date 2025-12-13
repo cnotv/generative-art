a76d022a882037745cbe1124e2a31ad54ddabefb
cnotv <giuseppe.leo@suse.com>
2025-12-13 18:10:13 +0100

    Add jump control to testing tools

--- src/views/Experiments/ToolsTest.vue
+++ src/views/Experiments/ToolsTest.vue
@@ -8,13 +8,14 @@ import {
 } from "@webgametoolkit/animation";
 import waterImage from "@/assets/water.png";
 import { createGame } from "@webgametoolkit/game";
+import { createControls } from "@webgametoolkit/controls";
 
 const chameleonConfig = {
   position: [0, -0.75, 0],
   scale: [0.05, 0.05, 0.05],
   restitution: -10,
   boundary: 0.5,
-  type: "kinematicPositionBased",
+  // type: "kinematicPositionBased",
   hasGravity: false,
   castShadow: true,
   receiveShadow: true,
@@ -37,6 +38,29 @@ const setupConfig = {
 
 const gameState = shallowRef({ data: { score: 0 } });
 createGame({ data: { score: 0 } }, gameState, onUnmounted);
+
+const isJumping = shallowRef(false);
+const canJump = shallowRef(true);
+const handleJump = () => {
+  if (isJumping.value || !canJump.value) return;
+  isJumping.value = true;
+  canJump.value = false;
+};
+const bindings = {
+  mapping: {
+    keyboard: {
+      " ": "jump",
+    },
+    gamepad: {
+      a: "jump",
+    },
+    touch: {
+      tap: "jump",
+    },
+  },
+  onAction: () => handleJump(),
+};
+createControls(bindings);
 
 const canvas = ref(null);
 const init = async () => {
@@ -78,18 +102,42 @@ const init = async () => {
             action: () =>
               gameState.value.setData("score", (gameState.value.data.score || 0) + 1),
           },
+          {
+            action: () => {
+              const jumpSpeed = 2;
+              if (isJumping.value) {
+                chameleon.mesh.position.y +=
+                  Math.sin((Date.now() * 0.01) / jumpSpeed) * 0.2;
+              } else {
+                // chameleon.mesh.position.y -=
+                //   Math.sin(Date.now() * 0.01 / jumpSpeed) * 0.2;
+              }
+
+              if (chameleon.mesh.position.y <= chameleonConfig.position[1] + 0.1) {
+                isJumping.value = false;
+                canJump.value = true;
+                chameleon.mesh.position.y = chameleonConfig.position[1];
+              }
+            },
+          },
         ],
       });
     },
   });
 };
 
-onMounted(async () => init());
+onMounted(async () => {
+  init();
+  window.addEventListener("resize", init);
+});
+onUnmounted(() => window.removeEventListener("resize", init));
 </script>
 
 <template>
   <canvas ref="canvas"></canvas>
   <div v-if="gameState" class="ui">
+    <p>Jumping: {{ isJumping }}</p>
+    <p>Can jump? {{ canJump }}</p>
     <h1>Loops: {{ gameState.data.score || 0 }}</h1>
   </div>
 </template>
@@ -102,7 +150,25 @@ canvas {
 }
 .ui {
   position: relative;
-  text-align: center;
-  font-size: 3em;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  font-size: 18px;
+  line-height: 1.2em;
+  justify-self: center;
+}
+
+h1 {
+  font-size: 4em;
+}
+
+p {
+  font-size: 2em;
+}
+
+@media (max-width: 600px) {
+  .ui {
+    line-height: 0.2em;
+  }
 }
 </style>
