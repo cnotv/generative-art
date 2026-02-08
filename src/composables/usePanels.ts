bab66453b54a14ce99199ca9e4881dfcbb8fa8d7
cnotv <giuseppe.leo@suse.com>
2026-02-08 16:12:03 +0000

    Set config query to false instead of undefined

--- src/composables/usePanels.ts
+++ src/composables/usePanels.ts
@@ -23,7 +23,7 @@ export const usePanels = () => {
     const { path, query } = route;
     const newQuery = {
       ...query,
-      config: panel === 'config' ? 'true' : undefined,
+      config: panel === 'config' ? 'true' : 'false',
     };
 
     router.push({ path, query: newQuery });
